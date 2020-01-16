e1442443ef45b71b74da41c55f34200d362033ea
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 15:15:29 -0300

    Added method subscriptionPromotionCode() to \IntellivoidSubscriptionManager\Utilities > Converter

--- src/IntellivoidSubscriptionManager/Utilities/Converter.php
+++ src/IntellivoidSubscriptionManager/Utilities/Converter.php
@@ -9,5 +9,16 @@
      */
     class Converter
     {
-
+        /**
+         * Converts promotion code to standard promotion
+         *
+         * @param string $input
+         * @return string
+         */
+        public static function subscriptionPromotionCode(string $input): string
+        {
+            $input = strtoupper($input);
+            $input = str_ireplace(' ', '_', $input);
+            return $input;
+        }
     }
