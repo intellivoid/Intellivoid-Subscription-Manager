8bcb9517e23fcef35cfefefff4593e5730947af3
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 12:30:13 -0300

    Added method getSubscriptionPlan() to \IntellivoidSubscriptionManager\Managers > PlanManager

--- src/IntellivoidSubscriptionManager/Managers/PlanManager.php
+++ src/IntellivoidSubscriptionManager/Managers/PlanManager.php
@@ -4,7 +4,14 @@
     namespace IntellivoidSubscriptionManager\Managers;
 
 
+    use IntellivoidSubscriptionManager\Abstracts\SearchMethods\SubscriptionPlanSearchMethod;
+    use IntellivoidSubscriptionManager\Exceptions\DatabaseException;
+    use IntellivoidSubscriptionManager\Exceptions\InvalidSearchMethodException;
+    use IntellivoidSubscriptionManager\Exceptions\SubscriptionPlanNotFoundException;
     use IntellivoidSubscriptionManager\IntellivoidSubscriptionManager;
+    use IntellivoidSubscriptionManager\Objects\SubscriptionPlan;
+    use msqg\QueryBuilder;
+    use ZiProto\ZiProto;
 
     /**
      * Class PlanManager
@@ -26,5 +33,65 @@
             $this->intellivoidSubscriptionManager = $intellivoidSubscriptionManager;
         }
 
+        /**
+         * Returns a Subscription Plan from the database
+         *
+         * @param string $search_method
+         * @param string $value
+         * @return SubscriptionPlan
+         * @throws DatabaseException
+         * @throws InvalidSearchMethodException
+         * @throws SubscriptionPlanNotFoundException
+         */
+        public function getSubscriptionPlan(string $search_method, string $value): SubscriptionPlan
+        {
+            switch($search_method)
+            {
+                case SubscriptionPlanSearchMethod::byId:
+                    $search_method = $this->intellivoidSubscriptionManager->getDatabase()->real_escape_string($search_method);
+                    $value = (int)$value;
+                    break;
 
+                case SubscriptionPlanSearchMethod::byPublicId:
+                    $search_method = $this->intellivoidSubscriptionManager->getDatabase()->real_escape_string($search_method);
+                    $value = $this->intellivoidSubscriptionManager->getDatabase()->real_escape_string($value);
+                    break;
+
+                default:
+                    throw new InvalidSearchMethodException();
+            }
+
+            $Query = QueryBuilder::select('subscription_plans', [
+                'id',
+                'public_id',
+                'application_id',
+                'plan_name',
+                'features',
+                'initial_price',
+                'cycle_price',
+                'billing_cycle',
+                'status',
+                'flags',
+                'last_updated',
+                'created_timestamp'
+            ], $search_method, $value);
+            $QueryResults = $this->intellivoidSubscriptionManager->getDatabase()->query($Query);
+
+            if($QueryResults == false)
+            {
+                throw new DatabaseException($Query, $this->intellivoidSubscriptionManager->getDatabase()->error);
+            }
+            else
+            {
+                if($QueryResults->num_rows !== 1)
+                {
+                    throw new SubscriptionPlanNotFoundException();
+                }
+
+                $Row = $QueryResults->fetch_array(MYSQLI_ASSOC);
+                $Row['features'] = ZiProto::decode($Row['features']);
+                $Row['flags'] = ZiProto::decode($Row['flags']);
+                return SubscriptionPlan::fromArray($Row);
+            }
+        }
     }
