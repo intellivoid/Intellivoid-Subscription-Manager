5ee2864c49fd0ef366fc2cc904b6be53eca32daf
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 15:23:53 -0300

    Added method SubscriptionPromotionPublicID() to \IntellivoidSubscriptionManager\Utilities > Hashing

--- src/IntellivoidSubscriptionManager/Utilities/Hashing.php
+++ src/IntellivoidSubscriptionManager/Utilities/Hashing.php
@@ -9,5 +9,15 @@
      */
     class Hashing
     {
-
+        /**
+         * Calculates a unique public ID for the subscription
+         *
+         * @param int $subscription_plan_id
+         * @param string $promotion_code
+         * @return string
+         */
+        public static function SubscriptionPromotionPublicID(int $subscription_plan_id, string $promotion_code): string
+        {
+            return hash('crc32b', $subscription_plan_id) . hash('sha256', $promotion_code);
+        }
     }
