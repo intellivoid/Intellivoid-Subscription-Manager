e56a3ef4a5ac26c2cc49ee355b37e553bc7cbf86
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 15:23:27 -0300

    Added Class Hashing to \IntellivoidSubscriptionManager\Utilities

---
+++ src/IntellivoidSubscriptionManager/Utilities/Hashing.php
@@ -0,0 +1,13 @@
+<?php
+
+
+    namespace IntellivoidSubscriptionManager\Utilities;
+
+    /**
+     * Class Hashing
+     * @package IntellivoidSubscriptionManager\Utilities
+     */
+    class Hashing
+    {
+
+    }
