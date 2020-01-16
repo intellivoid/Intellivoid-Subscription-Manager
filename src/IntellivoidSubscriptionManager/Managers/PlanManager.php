b342cfd129e4c876dcb68e678cae28e9c06eb26a
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 12:10:39 -0300

    Added InvalidSearchMethodException to \IntellivoidSubscriptionManager\Exceptions

---
+++ src/IntellivoidSubscriptionManager/Managers/PlanManager.php
@@ -0,0 +1,28 @@
+<?php
+
+
+    namespace IntellivoidSubscriptionManager\Managers;
+
+
+    use IntellivoidSubscriptionManager\IntellivoidSubscriptionManager;
+
+    /**
+     * Class PlanManager
+     * @package IntellivoidSubscriptionManager\Managers
+     */
+    class PlanManager
+    {
+        /**
+         * @var IntellivoidSubscriptionManager
+         */
+        private $intellivoidSubscriptionManager;
+
+        /**
+         * PlanManager constructor.
+         * @param IntellivoidSubscriptionManager $intellivoidSubscriptionManager
+         */
+        public function __construct(IntellivoidSubscriptionManager $intellivoidSubscriptionManager)
+        {
+            $this->intellivoidSubscriptionManager = $intellivoidSubscriptionManager;
+        }
+    }
