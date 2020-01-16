a1bc0d7579036e1886663062c4faa778e35bc8fb
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 12:06:36 -0300

    Added msqg requirement

--- src/IntellivoidSubscriptionManager/IntellivoidSubscriptionManager.php
+++ src/IntellivoidSubscriptionManager/IntellivoidSubscriptionManager.php
@@ -9,6 +9,11 @@
 
     $LocalDirectory = __DIR__ . DIRECTORY_SEPARATOR;
 
+    if(class_exists('msqg\msqg') == false)
+    {
+        include_once(__DIR__ . DIRECTORY_SEPARATOR . 'msqg' . DIRECTORY_SEPARATOR . 'msqg.php');
+    }
+
     if(class_exists('acm\acm') == false)
     {
         include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
---
+++ src/IntellivoidSubscriptionManager/Managers/SubscriptionManager.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace IntellivoidSubscriptionManager\Managers;
+
+
+class SubscriptionManager
+{
+
+}
