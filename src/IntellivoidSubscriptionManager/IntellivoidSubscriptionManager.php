78b2e77fab24806625cbf99d253127a8f275ee18
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 11:57:48 -0300

    Initial Commit

---
+++ src/IntellivoidSubscriptionManager/IntellivoidSubscriptionManager.php
@@ -0,0 +1,80 @@
+<?php
+
+
+    namespace IntellivoidSubscriptionManager;
+
+    use acm\acm;
+    use Exception;
+    use mysqli;
+
+    $LocalDirectory = __DIR__ . DIRECTORY_SEPARATOR;
+
+    if(class_exists('acm\acm') == false)
+    {
+        include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
+    }
+
+    include($LocalDirectory . 'AutoConfig.php');
+
+    class IntellivoidSubscriptionManager
+    {
+        /**
+         * @var mixed
+         */
+        private $DatabaseConfiguration;
+
+        /**
+         * @var mysqli
+         */
+        private $database;
+
+        /**
+         * @var acm
+         */
+        private $acm;
+
+        /**
+         * Constructs IntellivoidSubscriptionManager
+         *
+         * IntellivoidSubscriptionManager constructor.
+         * @throws Exception
+         */
+        public function __construct()
+        {
+            $this->acm = new acm(__DIR__, 'Intellivoid Subscription Manager');
+            $this->DatabaseConfiguration = $this->acm->getConfiguration('Database');
+
+            $this->database = new mysqli(
+                $this->DatabaseConfiguration['Host'],
+                $this->DatabaseConfiguration['Username'],
+                $this->DatabaseConfiguration['Password'],
+                $this->DatabaseConfiguration['Name'],
+                $this->DatabaseConfiguration['Port']
+            );
+
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDatabaseConfiguration()
+        {
+            return $this->DatabaseConfiguration;
+        }
+
+        /**
+         * @return mysqli
+         */
+        public function getDatabase(): mysqli
+        {
+            return $this->database;
+        }
+
+        /**
+         * @return acm
+         */
+        public function getAcm(): acm
+        {
+            return $this->acm;
+        }
+    }
