f011e955f3ace317557b733f64b85fd8f6def3dc
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 16:09:04 -0300

    Added constructor to SubscriptionManager in \IntellivoidSubscriptionManager\Managers

--- src/IntellivoidSubscriptionManager/Managers/SubscriptionManager.php
+++ src/IntellivoidSubscriptionManager/Managers/SubscriptionManager.php
@@ -1,10 +1,28 @@
 <?php
 
 
-namespace IntellivoidSubscriptionManager\Managers;
+    namespace IntellivoidSubscriptionManager\Managers;
 
 
-class SubscriptionManager
-{
+    use IntellivoidSubscriptionManager\IntellivoidSubscriptionManager;
 
-}
+    /**
+     * Class SubscriptionManager
+     * @package IntellivoidSubscriptionManager\Managers
+     */
+    class SubscriptionManager
+    {
+        /**
+         * @var IntellivoidSubscriptionManager
+         */
+        private $intellivoidSubscriptionManager;
+
+        /**
+         * SubscriptionManager constructor.
+         * @param IntellivoidSubscriptionManager $intellivoidSubscriptionManager
+         */
+        public function __construct(IntellivoidSubscriptionManager $intellivoidSubscriptionManager)
+        {
+            $this->intellivoidSubscriptionManager = $intellivoidSubscriptionManager;
+        }
+    }
