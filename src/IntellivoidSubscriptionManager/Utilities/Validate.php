0fa9ad0072ad3fe663f43b790231c783751c2eae
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-16 15:05:28 -0300

    Created class Validate in \IntellivoidSubscriptionManager\Utilities

---
+++ src/IntellivoidSubscriptionManager/Utilities/Validate.php
@@ -0,0 +1,13 @@
+<?php
+
+
+    namespace IntellivoidSubscriptionManager\Utilities;
+
+    /**
+     * Class Validate
+     * @package IntellivoidSubscriptionManager\Utilities
+     */
+    class Validate
+    {
+
+    }
