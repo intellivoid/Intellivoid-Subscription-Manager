6f6aa86a25af8905d4959670b79fabefa78d2185
netkas <netkas@intellivoid.info>
2020-01-29 23:00:31 -0300

    Added static method featuresToSA() in \IntellivoidSubscriptionManager\Utilities > Converter

--- src/IntellivoidSubscriptionManager/Utilities/Converter.php
+++ src/IntellivoidSubscriptionManager/Utilities/Converter.php
@@ -3,6 +3,8 @@
 
     namespace IntellivoidSubscriptionManager\Utilities;
 
+    use IntellivoidSubscriptionManager\Objects\Subscription\Feature;
+
     /**
      * Class Converter
      * @package IntellivoidSubscriptionManager\Utilities
@@ -20,5 +22,56 @@
             $input = strtoupper($input);
             $input = str_ireplace(' ', '_', $input);
             return $input;
+        }
+
+        /**
+         * Converts array of features to a simplified array
+         *
+         * @param array $features
+         * @param bool $is_object
+         * @return array
+         */
+        public static function featuresToSA(array $features, bool $is_object=True): array
+        {
+            $Results = array();
+
+            if($is_object)
+            {
+                /** @var Feature $feature */
+                foreach($features as $feature)
+                {
+                    $Results[$feature->Name] = $feature->Value;
+                }
+                
+                return $Results;
+            }
+
+            foreach($features as $feature)
+            {
+                $name = null;
+                $value = null;
+
+                if(isset($feature['Name']))
+                {
+                    $name = $feature['Name'];
+                }
+
+                if(isset($feature['name']))
+                {
+                    $name = $feature['name'];
+                }
+
+                if(isset($feature['Value']))
+                {
+                    $value = $feature['Value'];
+                }
+
+                if(isset($feature['value']))
+                {
+                    $value = $feature['value'];
+                }
+
+                $Results[$name] = $value;
+            }
         }
     }
